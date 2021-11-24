890a9d554d898a6fc2b9bb83d9c3d2a69b296a2e
Michiel Lowijs <mlowijs@gmail.com>
2021-11-24 21:35:08 +0100

    add some packages

--- packages.nix
+++ packages.nix
@@ -13,19 +13,26 @@
     gnome-terminal.enable = true;
 
     xwayland.enable = true;
+  
+    npm.enable = true;
   };
 
   environment.systemPackages = with pkgs; [
     btrfs-progs
 
     gnome.gnome-tweaks
+    gnomeExtensions.sound-output-device-chooser
+    gnomeExtensions.bluetooth-quick-connect
     
     firefox-wayland
+    slack
+    bitwarden
 
     vscode
     jetbrains.rider
     dotnet-sdk
     dotnet-runtime
     dotnet-aspnetcore
+    nodejs-16_x
   ];
 }
